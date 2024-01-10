12eec4eee8501d066a00ba50da23c7f58ebe920a
Raziel Rashad <raziel1q2w3e@gmail.com>
2024-01-10 11:28:54 +0200

    first commit

---
+++ src/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './style.module.css';
+
+export default function Header() {
+    return (
+        <div className={styles.header}>
+
+            <div className={styles.user}>
+
+            </div>
+
+        </div>
+    )
+}
