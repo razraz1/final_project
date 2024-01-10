5226ea566f69a05231b267d4d5b5004f150bb70f
yacov binik <n0546003191@gmail.com>
2024-01-10 11:54:49 +0200

    initialization of react

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,14 +1,9 @@
-import React from 'react';
+import React from 'react'
 import styles from './style.module.css';
 
 export default function Header() {
     return (
-        <div className={styles.header}>
-
-            <div className={styles.user}>
-
-            </div>
-
-        </div>
+        <div>Header</div>
     )
 }
+
