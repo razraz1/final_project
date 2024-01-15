f1981b487d607a025bea576b189b5546c8ed7bf9
yacov binik <n0546003191@gmail.com>
2024-01-15 09:16:32 +0200

    add Routes

--- frontend/src/Content/Content.jsx
+++ frontend/src/Content/Content.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import Inbox from '../Inbox/Inbox'
-import Toolbar from '../Toolbar/Toolbar'
-import Editing from '../Editing/Editing'
-import styles from './style.module.css'
-import Outbox from '../Outbox/Outbox'
+import React from "react";
+import Inbox from "../Inbox/Inbox";
+import Toolbar from "../Toolbar/Toolbar";
+import Editing from "../Editing/Editing";
+import styles from "./style.module.css";
+import Outbox from "../Outbox/Outbox";
+import { Route, Routes } from "react-router-dom";
 // import Outbox from '../Outbox/Outbox'
 
 // import Editing from '../Editing/Editing'
 
 export default function Content() {
-    return (
-        <div className={styles.content}>
-            <Toolbar />
-            <Inbox />
-            {/* <Editing /> */}
-            {/* <Outbox /> */}
-        </div>
-    )
+  return (
+    <div className={styles.content}>
+      <Toolbar />
+      <Routes>
+        <Route path="/inbox" element={<Inbox />} />
+        <Route path="/*" element={<Inbox />} />
+        <Route path="/editing" element={<Editing />} />
+        <Route path="/outbox" element={<Outbox />} />
+      </Routes>
+    </div>
+  );
 }
